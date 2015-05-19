ec2aa2f5963945ebee8255acb68de0dd0eee01c3
Florian Grundig <flg@trifork.com>
2015-05-19 10:03:47 +0200

    updated google cloud terraform scripts (incomplete)

--- templates/gcc/master.tf
+++ templates/gcc/master.tf
@@ -1,18 +1,3 @@
- account_file="cs-cisco-ca214d29dc66.json"
- gce_ssh_user="554985525398-p9se88l5e3fupvj1v8t6tujq5qsumh1q.apps.googleusercontent.com"
- gce_ssh_private_key_file="pkey"
- region ="europe-west1"
- zone="europe-west1-d"
- project="cs-cisco"
- image="ubuntu-os-cloud/ubuntu-1404-trusty-v20150128"
- master_machine_type="n1-standard-2"
- slave_machine_type= "n1-standard-4"
- network= "10.20.30.0/24"
- localaddress="92.111.228.8/32"
- name="mymesoscluster"
- masters= "1"
-
-
 
 resource "google_compute_instance" "mesos-master" {
     count = "${var.nodes}"
@@ -42,7 +27,7 @@ resource "google_compute_instance" "mesos-master" {
 
     # define default connection for remote provisioners
     connection {
-      user = "${var.gce_ssh_user}"
-      key_file = "${var.gce_ssh_private_key_file}"
+      user = "ubuntu"
+      key_file = "~/.ssh/triforkse_rsa.pub"
     }
 }
