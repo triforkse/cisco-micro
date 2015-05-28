6a649ed4a7685883f48201784179bbc897f4463b
Erik Peldan <epe@trifork.com>
2015-05-28 11:24:05 +0200

    Try to separate side effects whenever possible

--- micro/config.go
+++ micro/config.go
@@ -17,42 +17,71 @@ type Config struct {
 	path   string
 }
 
-func readVars(path string) (*ConfigVars, error) {
+// pure function
+func parseJsonBytes(bytes []byte) (*ConfigVars, error) {
 
-	var bytes []byte
 	var err error
-
-	bytes, _ = ioutil.ReadFile(path)
-
 	var config ConfigVars
+
 	err = json.Unmarshal(bytes, &config)
 	if err != nil {
-		log.Printf("Can't read config %s", path)
 		return nil, err
 	}
 
 	return &config, nil
 }
 
+// pure function
 func isConfigPath(path string) bool {
 	return strings.HasSuffix(path, ".json")
 }
 
-func allConfigPathsInDirectory(dirPath string) []string {
-
+// pure function
+func validConfigPaths(paths []string) []string {
 	var configPaths = []string{}
 
-	files, _ := ioutil.ReadDir(dirPath)
-	for _, f := range files {
-		name := f.Name()
-		if isConfigPath(name) {
-			configPaths = append(configPaths, name)
+	for _, f := range paths {
+		if isConfigPath(f) {
+			configPaths = append(configPaths, f)
 		}
 	}
 
 	return configPaths
 }
 
+// impure; file I/O
+func readVars(path string) (*ConfigVars, error) {
+
+	var bytes []byte
+
+	bytes, _ = ioutil.ReadFile(path)
+	vars, err := parseJsonBytes(bytes)
+	if err != nil {
+		log.Printf("Can't read config %s", path)
+	}
+
+	return vars, err
+}
+
+// impure; file I/O
+func allConfigPathsInDirectory(dirPath string) []string {
+	return validConfigPaths(allFilePathsInDirectory(dirPath))
+}
+
+// impure; file I/O
+func allFilePathsInDirectory(dirPath string) []string {
+	var paths = []string{}
+
+	files, _ := ioutil.ReadDir(dirPath)
+	for _, f := range files {
+		name := f.Name()
+		paths = append(paths, name)
+	}
+
+	return paths
+}
+
+// impure; file I/O
 func readConfigs(directory string) []Config {
 	var configs = []Config{}
 	var paths = []string{}
